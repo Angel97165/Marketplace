707ea7752e728d0311c0dd08d4dcc76ef80a738a
Angel97165 <152416467+Angel97165@users.noreply.github.com>
2023-12-20 17:58:42 +0200

    comituk

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let productsGird = document.getElementById('producta-grid');
+let productsGird = document.getElementById('products-gird');
 let productArray = [];
 let xhr = new XMLHttpRequest();
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
@@ -7,17 +7,17 @@ xhr.open('GET', url + '/products');
 xhr.responseType = 'json';
 xhr.onload = function(){
     let products = xhr.response;
-    productsGird.innerHTMLt = null;
+    productsGird.innerHTML = null;
     products.forEach(element => {
         productArray.push(element);
         let pElem = document.createElement('div');
         pElem.classList.add('product')
         pElem.innerHTML = `
-            <h2 class="product-name">${p.name}</h2>
-            <img class="prodect-photo" src="${element.photo_url}" alt="${element.name}">
+            <h2 class="product-name">${element.name}</h2>
+            <img class="product-photo" src="${element.photo_url}" alt="${element.name}">
             <p class="product-price"><b>Price: </b>${element.price}</p>
             <p class="product-description"><b>Description: </b>${element.description}</p>
-            <a href="">Seller profile</a>
+            <a href="userProfile.html?id=${element.author_id}">Seller profile</a>
             <button>Buy</button>
         `;
         productsGird.append(pElem);
